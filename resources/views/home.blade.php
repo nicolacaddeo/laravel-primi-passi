07d7f9b7a398aab6acb03c2976ce80bfecf8989e
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-10-27 15:17:53 +0200

    dynamically print an array on page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,34 @@
+@php
+    $friends = ['Froxy', 'Sumi', 'Matte', 'Edo', 'Mike', 'Xado'];
+@endphp
+
 <h1>Hello World!</h1>
+
+<div class="list-container">
+    <h4>My Friends:</h4>
+    <ul>
+        @foreach ($friends as $friend)
+            <li>{{ $friend }}</li>
+        @endforeach
+    </ul>
+</div>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Segoe UI', sans-serif;
+    }
+
+    li {
+        list-style-type: none;
+    }
+
+    .list-container {
+        padding: 2rem
+    }
+</style>
